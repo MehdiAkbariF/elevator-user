c1bc95e5a3eb091217ab9d6f6001dc15bf0f0b8d
MehdiAkbariF <mehdiakbarideveloper@gmail.com>
2026-08-08 13:08:13 +0330

    request service page added

---
+++ app/request-service.tsx
@@ -0,0 +1,8 @@
+// app/request-service.tsx
+
+import RequestServiceScreen from "@/src/features/catalog/screens/RequestServiceScreen"; // ایمپورت پیش‌فرض بدون کروشه {}
+import React from "react";
+
+export default function RequestServiceRoute() {
+  return <RequestServiceScreen />;
+}
